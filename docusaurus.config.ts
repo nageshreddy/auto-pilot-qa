686c3ecddf0dde4dc1aa44c7304954739b30b66b
Nagesh Mallareddy <nageshreddy@hotmail.co.uk>
2025-06-11 18:08:37 +0100

    Updated logo image

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'AutoPilotQA',
+  title: 'AutoPilot QA',
   tagline: 'Automation with bite.',
   favicon: 'img/favicon.ico',
 
